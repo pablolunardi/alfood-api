0a6244719508a1306b4516f2fa2bc1fd665f6a0e
Pablo Augusto Lunardi <pablo.lunardi@compasso.com.br>
2021-01-25 10:24:32 -0300

    Criação projeto

---
+++ src/main/java/com/lunardi/alfood/AlfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.lunardi.alfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lunardi/alfood/AlfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunardi.alfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
