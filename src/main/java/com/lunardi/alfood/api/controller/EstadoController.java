7b4a6543d50747c71a9e0e34173504e00cf73ce8
Pablo Augusto Lunardi <pablolunardi@gmail.com>
2021-02-17 21:48:49 -0300

    Adicionado dependência jackson para suporte ao formato xml

---
+++ src/main/java/com/lunardi/alfood/api/controller/EstadoController.java
@@ -0,0 +1,25 @@
+package com.lunardi.alfood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lunardi.alfood.domain.model.Estado;
+import com.lunardi.alfood.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@GetMapping
+	public List<Estado> listar() {
+		return estadoRepository.listar();
+	}
+
+}
