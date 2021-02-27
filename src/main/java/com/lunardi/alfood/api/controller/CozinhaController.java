ec825080199edc15421ab2624b63309a02069989
Pablo Augusto Lunardi <pablolunardi@gmail.com>
2021-02-27 15:37:08 -0300

    Inserido endpoints de criação, atualização e remoção

--- src/main/java/com/lunardi/alfood/api/controller/CozinhaController.java
+++ src/main/java/com/lunardi/alfood/api/controller/CozinhaController.java
@@ -2,12 +2,19 @@ package com.lunardi.alfood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lunardi.alfood.api.model.CozinhasXmlWrapper;
@@ -40,6 +47,41 @@ public class CozinhaController {
 		}
 		
 		return ResponseEntity.ok(cozinha);
+	}
+	
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		
+		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinhaAtual == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+		
+		cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+		
+		return ResponseEntity.ok(cozinhaAtual);
+	}
+	
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Void> remover(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinha == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cozinhaRepository.remover(cozinha);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
