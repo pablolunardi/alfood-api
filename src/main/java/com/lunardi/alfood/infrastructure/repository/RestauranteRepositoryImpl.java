b45702cbfa8380e720c77dd0e920e32be34f5669
Pablo Augusto Lunardi <pablolunardi@gmail.com>
2021-03-17 09:48:28 -0300

    Testes com criteria e com jpql

--- src/main/java/com/lunardi/alfood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/lunardi/alfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,19 +1,27 @@
 package com.lunardi.alfood.infrastructure.repository;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import com.lunardi.alfood.domain.model.Restaurante;
-import com.lunardi.alfood.domain.repository.RestauranteRepository;
+import com.lunardi.alfood.domain.repository.RestauranteRepositoryQueries;
 
-//@Component
-public class RestauranteRepositoryImpl   {
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
 	@PersistenceContext
 	EntityManager manager;
@@ -44,6 +52,61 @@ public class RestauranteRepositoryImpl   {
 		}
 		
 		manager.remove(restaurante);
+	}
+
+	//@Override
+//	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+//		
+//		var jpql = new StringBuilder("from Restaurante where 0 = 0 ");
+//		var parametros = new HashMap<String, Object>();
+//		
+//		if (StringUtils.hasLength(nome)) {
+//			jpql.append("and nome like :nome ");
+//			parametros.put("nome", "%" + nome + "%");
+//		}
+//		
+//		if (taxaFreteInicial != null) {
+//			jpql.append("and taxaFrete >= :taxaInicial ");
+//			parametros.put("taxaInicial", taxaFreteInicial);
+//		}
+//		
+//		if (taxaFreteFinal != null) {
+//			jpql.append("and taxaFrete <= :taxaFinal ");
+//			parametros.put("taxaFinal", taxaFreteFinal);
+//		}
+//		
+//		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+//		
+//		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+//				
+//		return query.getResultList();
+//	}
+	
+	@Override
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		var predicates = new ArrayList<Predicate>();
+		
+		if (StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		}
+		
+		if (taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
+		
+		if (taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
+		
+		criteria.where(predicates.toArray(new Predicate[0]));
+		
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
+		return query.getResultList();
 	}
 
 }
