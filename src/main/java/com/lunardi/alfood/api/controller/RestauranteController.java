30d88f8f2e76d46580336b333b11279f0036b137
Pablo Augusto Lunardi <pablolunardi@gmail.com>
2021-04-14 23:39:36 -0300

    Ajuste exceções Ajuste handler para estender ResponseEntityExceptionHandler

--- src/main/java/com/lunardi/alfood/api/controller/RestauranteController.java
+++ src/main/java/com/lunardi/alfood/api/controller/RestauranteController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lunardi.alfood.domain.exception.CozinhaNaoEncontradaException;
-import com.lunardi.alfood.domain.exception.EntidadeNaoEncontradaException;
 import com.lunardi.alfood.domain.exception.NegocioException;
 import com.lunardi.alfood.domain.model.Restaurante;
 import com.lunardi.alfood.domain.repository.RestauranteRepository;
--- src/main/java/com/lunardi/alfood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/lunardi/alfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,58 +2,62 @@ package com.lunardi.alfood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.lunardi.alfood.domain.exception.EntidadeEmUsoException;
 import com.lunardi.alfood.domain.exception.EntidadeNaoEncontradaException;
 import com.lunardi.alfood.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<Problema> tratarEntidadeNaoEncontradoException(EntidadeNaoEncontradaException e) {
+	public ResponseEntity<Object> tratarEntidadeNaoEncontradoException(EntidadeNaoEncontradaException ex,
+			WebRequest request) {
 		Problema problema = Problema.builder()
 				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
+				.mensagem(ex.getMessage()).build();
 		
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-				.body(problema);
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<Problema> tratarNegocioException(NegocioException e) {
+	public ResponseEntity<Object> tratarNegocioException(NegocioException ex, WebRequest request) {
 		Problema problema = Problema.builder()
 				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
+				.mensagem(ex.getMessage()).build();
 		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(problema);
-	}
-	
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<Problema> tratarHttpMediaTypeNotSupportedException() {
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem("O tipo de mídia não é aceito").build();
-		
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-				.body(problema);
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<Problema> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
+	public ResponseEntity<Object> tratarEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
 		Problema problema = Problema.builder()
 				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
+				.mensagem(ex.getMessage()).build();
+	
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), HttpStatus.CONFLICT, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
 		
-		return ResponseEntity.status(HttpStatus.CONFLICT)
-				.body(problema);
+		if (body == null) {
+			body = Problema.builder().dataHora(LocalDateTime.now())
+					.mensagem(status.getReasonPhrase()).build();
+		} else if (body instanceof String) {
+			body = Problema.builder().dataHora(LocalDateTime.now())
+					.mensagem((String) body);
+		}
+		
+		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 	
 }
